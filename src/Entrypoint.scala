cd19a492d2ca4d8fa1bef2ec45237d9a9c49ae84
ColinWojciechowski <C.Wojciechowski@gmx.net>
2016-12-16 07:14:04 +0100

    refactored "Nullstellensuche" cleaned up Entrypoint and Person

--- src/Entrypoint.scala
+++ src/Entrypoint.scala
@@ -4,26 +4,11 @@
 object Entrypoint {
   val student = new Person("Karl Heinz", 21)
   def main(args: Array[String]): Unit = {
-    student.age = 30
-    student.name = "Jonny Banana"
-    println(student name)
-    println(student age)
-    println(createName(student name))
+    println(student.createName(student name))
     println()
-    var testList: Seq[Int] = Seq(1,8,5,2,6,4,9,7,3)
-    testList  ++= Seq(9,4,5,2,3,5,8,7)
-    println(testList contains 41)
-    println(testList size)
-
     val mathe = new Mathe()
     println(mathe.factorial(4))
-    print(mathe.theZero(6, 5))
-  }
-
-  def createName(name:String):String = {
-    def loop(i: Int, s: String): String =
-      if (i < name.length) loop(i + 1, s+name.charAt(i)) else s
-    loop(0, "")
+    mathe.checkForTheZeros(6,-5)
   }
 
 
--- src/Person.scala
+++ src/Person.scala
@@ -3,13 +3,16 @@
   */
 class Person(private var nameOfBirth: String, private var curAge: Int){
 
+  def createName(name:String):String = {
+    def loop(i: Int, s: String): String =
+      if (i < name.length) loop(i + 1, s+name.charAt(i)) else s
+    loop(0, "")
+  }
+
   //Getter
+  def name: String = nameOfBirth
   def age = curAge
   //Setter
-  def age_=(value:Int) = curAge = value
-
-  def name: String = nameOfBirth
   def name_=(value:String) = nameOfBirth = value
-
-
+  def age_=(value:Int) = curAge = value
 }
